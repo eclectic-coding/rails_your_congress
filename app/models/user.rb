2e30daad1b0826df5d9681a6643a0a8141571554
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-03 12:21:49 -0400

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+has_person_name
+has_one_attached :avatar
+
+end
