db330f296d3a0a179329f255a0c3d93a92518e1b
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-05 15:59:11 -0400

    Complete seantor show and index views

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -1,7 +1,10 @@
 class SenatorsController < ApplicationController
+
   def index
+    @senators = Senator.paginate(page: params[:page], per_page: 20)
   end
 
   def show
+    @senator = Senator.find_by_member_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'senators/index'
-  get 'senators/show'
   root to: 'static#home'
 
   devise_for :users
   resources :users, only: [:show]
 
   resources :representatives, only: %i[index show]
+  resources :senators, only: %i[index show]
 
 end
