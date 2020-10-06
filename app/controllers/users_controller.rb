eea0a368e111b1242f966e89986393ebf0934970
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-06 14:10:13 -0400

    Update devise for usernames

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by!(username: params[:username])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -120,11 +120,13 @@ ActiveRecord::Schema.define(version: 2020_10_05_171247) do
     t.datetime "locked_at"
     t.string "first_name"
     t.string "last_name"
+    t.string "username"
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
