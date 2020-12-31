3322702e03b9111df3c3014d9a7c1a989c25c67b
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-12-31 07:21:49 -0500

    Temporarily remove pg_search and searchbar

--- app/controllers/senators_controller.rb
+++ app/controllers/senators_controller.rb
@@ -1,17 +1,7 @@
 class SenatorsController < ApplicationController
 
   def index
-    if params[:query].present?
-      @senator_search = Senator.global_search(params[:query])
-      @senators = @senator_search.paginate(page: params[:page], per_page: 20)
-    else
       @senators = Senator.paginate(page: params[:page], per_page: 20)
-    end
-
-    respond_to do |format|
-      format.html
-      format.json { render json: { senators: @senators } }
-    end
   end
 
   def show
